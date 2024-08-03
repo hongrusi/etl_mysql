886ee3c22504bffd11ac660e135aaea77624c48c
hongrusi <hongrusi@gmail.com>
2024-08-04 00:10:34 +1000

    fix syntax error in etl.py

--- etl.py
+++ etl.py
@@ -2,33 +2,31 @@
 import os
 import requests
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 def extract(country: str = "United+States") -> dict:
-    """ This API extracts data from
-    http://universities.hipolabs.com
-    """
+ 
     API_URL = f"http://universities.hipolabs.com/search?country={country}"
     try:
-	    response = requests.get(API_URL)
-	    response.raise_for_status()
-	    data = response.json()
-	    return data
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        data = response.json()
+        return data
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
         return {}
 
 def transform(data:dict) -> pd.DataFrame:
-    """ Transforms the dataset into desired structure and filters"""
+  
     df = pd.DataFrame(data)
     print(f"Total Number of universities from API {len(data)}")
     
     df = df[df["name"].str.contains("California")]
     print(f"Number of universities in california {len(df)}")
     
-    df['domains'] = [','.join(map(str, l)) for l in df['domains']]
-    df['web_pages'] = [','.join(map(str, l)) for l in df['web_pages']]
+    df['domains'] = [','.join(map(str, i)) for i in df['domains']]
+    df['web_pages'] = [','.join(map(str, i)) for i in df['web_pages']]
     df = df.reset_index(drop=True)
     return df[["domains","country","web_pages","name"]]
 
